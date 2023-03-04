6fdf0694ec09d0b6c288fc1f1cdeec138c81ee8d
ramazzzini <ramazan98derbent@gmail.com>
2023-03-04 21:22:36 +0500

    dragdrop2

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+const task = document.querySelector('.task');
 
+task.addEventListener('click', createNewItem)
 item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+}
 function dragstart(event) {
     event.target.classList.add('hold')
     setTimeout(() => {
@@ -11,6 +20,33 @@ function dragstart(event) {
 }
 
 function dragend(event) {
-    event.target.classList.remove('hold')
-    event.target.classList.remove('hidden')
+    event.target.className = ('item')
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.append(item)
+    event.target.classList.remove('hovered')
+}
+
+function createNewItem() {
+    task.insertAdjacentHTML('beforebegin', `
+  <div class="row">
+    <div class="placeholder">
+      <div class="item" draggable="true">Перетащи меня</div>
+    </div>
+    <div class="placeholder"></div>
+    <div class="placeholder"></div>
+  </div>`)
 }
